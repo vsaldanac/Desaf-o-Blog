777100e569a1e77119c1c09ee64899760ca98f51
Veronica Saldana <v.saldana.c@gmail.com>
2020-02-09 02:20:56 -0300

    add bootstrap

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,7 @@ module PostsHelper
     def crudPostButtons(post)
         if user_signed_in?
             if current_user.admin?
-                return " | #{link_to 'Edit', edit_post_path(post) } | #{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }}".html_safe
+                return "#{link_to 'Edit', edit_post_path(post), class: 'text-white btn btn-success'} #{link_to 'Destroy', post, method: :delete, class: 'text-white btn btn-danger', data: { confirm: 'Are you sure?' }}".html_safe
             end
         end
     end
